86356c54bd526a2b43909ccf1a3eb256f007b30a
kiyotaki4 <marlinezxc@gmail.com>
2025-03-17 11:41:54 +0900

    cafesite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Tree from "./tree.jsx"
+function App() {
+
+  return (
+    <>
+      <Tree />
+    </>
+  )
+}
+
+export default App
---
+++ src/tree.jsx
@@ -0,0 +1,132 @@
+function Tree(){
+    return <div>
+        <div className="headerpage">
+            <nav className="navbar">
+                <img src="/logocafe.svg" alt="" />
+                <div className="row">
+                    <span>Stores</span>
+                    <span>Help & Support</span>
+                    <button>Download</button>
+                </div>
+            </nav>
+            <div className="herocontainer">
+                <h1><span>Order ahead</span><br /> and earn rewards for every purchase</h1>
+                <button>Discover now <img src="/arrowsvg.svg" alt="" /></button>
+                
+            </div>
+            <img src="/hero.svg" alt="" className="heroimg"/>
+                <img src="/background.svg" alt="" className="backimg"/>
+        </div>
+        <div className="framecontainer">
+            <div className="column">
+                <img src="/columnsvg1.svg" alt="" />
+                <h2>Order ahead, skip the line</h2>
+                <p>Feel like a rock star when you bypass the line and save a bunch of time.</p>
+            </div>
+            <div className="column">
+                <img src="/columnsvg2.svg" alt="" />
+                <h2>Bonus and discounts</h2>
+                <p>You can enjoy lots of discounts from the selected stores, or accumulate points.</p>
+            </div>
+            <div className="column">
+                <img src="/columnsvg3.svg" alt="" />
+                <h2>Share with your friends</h2>
+                <p>Send gifts to your friends and family, and pick up for your coworkers when they can't get away .</p>
+            </div>
+        </div>
+        <div className="phonecontainer">
+            <div className="text-container">
+            <p>Your daily pick-me-up</p>
+            <h2>How does <br />it work.</h2>
+            <span>Start choosing your favorite teas and snacks in your nearby stores and place your orders, accumulate points, share gifts and teas with your friends and co-workers, and finally use our T-Pass to get your free drinks.</span>
+            <div className="box"></div>
+            </div>
+            <img src="/phonesvg.svg" alt="" className="phonesvg"/>
+            <img src="/chipssvg.svg" alt="" className="chipssvg"/>
+            <img src="/cofesvg.svg" alt="" className="cofesvg"/>
+            <img src="/phone-content.svg" alt="" className="phone-content"/>
+            <div className="background"></div>
+        </div>
+        <div className="heropage">
+            <div className="box">
+            <div className="container-loyalty">
+                <img src="/loyalty.svg" alt="" />
+                <div className="column">
+                    <h2>Loyalty points</h2>
+                    <p>Collect points on each store and redeem them to change for Teas.</p>
+
+                </div>
+            </div>
+            <div className="container-exclusive">
+                <img src="/exclusive.svg" alt="" />
+                <div className="column">
+                    <h2>Exclusive Store Discounts</h2>
+                    <p>You can enjoy lots of discounts from the selected stores near you.</p>
+
+                </div>
+            </div>
+            <div className="container-drinks">
+                <img src="/drinks.svg" alt="" />
+                <div className="column">
+                    <h2>T.Pass Free Drinks</h2>
+                    <p>2 Teas for $5.99 each month. and you can carry them over to the next month.</p>
+
+                </div>
+            </div>
+            </div>
+            <img src="/hero2.svg" alt="" className="heroimg"/>
+        <div className="backgroundhero"></div>
+
+        </div>
+        <div className="sendgifts">
+            <div className="text-container">
+            <p>Please your friends</p>
+            <h2>Send gifts and <br />order for groups</h2>
+            <span>Send Gifts with a value for your friends and family and use it on their favorite stores. Be the rock star make Group Orders and pick up for your coworkers when they can't get away.</span>
+            <div className="box"></div>
+            </div>
+            <div className="background"></div>
+            <img src="/secondphone.svg" alt="" className="secondphone"/>
+            <img src="/strawberry.svg" alt="" className="strawberry"/>
+            <img src="/strawberrycoctail.svg" alt="" className="strawberrycoctail"/>
+        </div>
+        <div className="prefooter">
+            <img src="/homelogo.svg" alt="" />
+            <h1>Enjoy your afternoon <br />tea with a snack</h1>
+            <div className="content">
+            <div className="row">
+            <input type="text" placeholder="Enter mobile number"/>
+            <button>Send Link</button>
+            </div>
+            <div className="img-row">
+            <img src="/appstore.svg" alt="" />
+            <img src="/google.svg" alt="" />
+            </div>
+            </div>
+            
+        </div>
+        <div className="footer">
+            <img src="/logosvgwhite.svg" alt="" />
+            <div className="row">
+                <div className="column">
+                    <p>Company</p>
+                    <span>Merchant Portal</span>
+                </div>
+                <div className="column">
+                    <p>Support</p>
+                    <span>FAQ</span>
+                    <span>Terms of Service</span>
+                    <span>Privacy Policy</span>
+                </div>
+                <div className="column">
+                    <p>Follow Us</p>
+                    <span>Twitter</span>
+                    <span>Instagram</span>
+                    <span>Facebook</span>
+                </div>
+            </div>
+            <img src="/leaf-footer1.svg" alt="" className="leaf-footer1"/>
+        </div>
+    </div>
+}
+export default Tree
